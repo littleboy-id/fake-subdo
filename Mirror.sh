63897f03ccab4eeafc7cf1de528496b9abf1fa64
littleboy-id <71086492+littleboy-id@users.noreply.github.com>
2020-09-10 23:14:17 +0800

    Create Mirror.sh

---
+++ Mirror.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/bash
+
+#Warna
+b='\e[1;34m' #blue
+g='\e[1;32m' #green
+p='\e[1;35m' #purple
+c='\e[1;36m' #cyan
+r='\e[1;31m' #red
+w='\e[1;37m' #white
+y='\e[1;33m' #yellow
+
+banner(){
+clear
+echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
+  echo   Author: ./LittleBoy
+ echo    WA   :  +1 (669) 245-7368
+ echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
+
+}
+menu(){
+printf "
+$c Pilih :
+$y[1]$c Buat Fake Deface
+$y[2]$f Notify di Defacer.id
+$y[3]$p Exit
+
+"
+}
+banner
+menu
+read -p "=> " pa
+
+if [ $pa = 1 ]||[ $pa = satu ];
+then
+read -p "Nama web ?=> http://" domain
+read -p "Subdomainya (ex: taniacans) ?=> " subdo
+read -p "Jumlahnya? (1-9999) ?=> " jumlah
+read -p "filenya (tod.txt) ?=> " output
+count=1
+while [ $count -le $jumlah ]
+do
+printf "http://$subdo$RANDOM.$domain\n" >> $output
+echo "[$count] Proses... sabar ya --> $output"
+sleep 0;
+(( count++ ))
+done
+echo "Horeee Selesai!"
+echo
+echo
+echo
+echo " Silahkan pindah filenya"
+echo 
+echo " nih caranya :"
+echo "ketik ls"
+echo " kalau udah ,copas saja di defacer.id"
+echo -e $g "  (-) $output (-)"
+exit
+
+else
+exit
+
+fi
