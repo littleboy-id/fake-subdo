32f93d961e303dab123208459fdf3670b82c428d
littleboy-id <71086492+littleboy-id@users.noreply.github.com>
2020-09-11 09:16:48 +0800

    Update and rename sub.sh to subdo.sh

--- subdo.sh
+++ subdo.sh
@@ -12,16 +12,15 @@ y='\e[1;33m' #yellow
 banner(){
 clear
 echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
-  echo   Author:Tania
- echo    WA   :  meninggalkanjejak
+  echo   Author:./LittleBoy
  echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
 
 }
 menu(){
 printf "
-$c Pilih :
+$g Pilih :
 $y[1]$c Buat Fake Deface
-$y[2]$p Exit
+$y[2]$r Exit
 
 "
 }
@@ -31,26 +30,27 @@ read -p "=> " pa
 
 if [ $pa = 1 ]||[ $pa = satu ];
 then
-read -p "Nama web ?=> http://" domain
-read -p "Subdomainya (ex: taniacans) ?=> " subdo
-read -p "Jumlahnya? (1-9999) ?=> " jumlah
-read -p "filenya (tansmirror.txt) ?=> " output
+read -p "Nama Website Lu=> http://" domain
+read -p "Subdomain (ex: littleboy)=> " subdo
+read -p "Jumlah => " jumlah
+read -p "file (ex: list.txt) => " output
 count=1
 while [ $count -le $jumlah ]
 do
 printf "http://$subdo$RANDOM.$domain\n" >> $output
-echo "[$count]  Lagi  di  Proses  beb=> $output"
-sleep 000.1;
+echo "[$count]  Prosses Ya Syng :3=> $output"
+sleep 0;
 (( count++ ))
 done
-echo "[-_-]  Ciee , Udah  Selesai  [-_-]"
+echo " Udh Selesai Gann:) "
 echo
 echo
 echo
 echo " Silahkan pindah filenya"
 echo 
 echo " nih caranya :"
-echo "ketik : cp nama file /sdcard/"
+echo "ketik : ls untuk melihat file"
+echo "ketik : cp (ex: cp list.txt downloads)
 echo " kalau udah ,copas saja di defacer.id"
 echo -e $g "  (-) $output (-)"
 exit
